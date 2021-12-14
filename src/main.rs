2b1b68fe1b695afbadb224ad1e1b759b85113cbd
Bobby Keel <bobbykeel76@gmail.com>
2021-12-14 06:28:04 -0600

    more functions in rust, closures, math operations, list, arrays, etc

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use core::num;
 use std::{i8, i16, i32, i64, u8, u16, u32, u64, isize, usize, f32, f64};
 
-// use std::io::stdin;
+use std::io::stdin;
 
 fn main() {
     println!("Hello World");
@@ -60,7 +61,7 @@ fn main() {
     println!("5 + 4 = {}", 5 + 4);
     println!("5 - 4 = {}", 5 - 4);
     println!("5 * 4 = {}", 5 * 4);
-    println!("5 / 4 = {}", 5 / 4);
+    println!("5 / 4 = {}", 5f32 / 4f32);
     println!("5 % 4 = {}", 5 % 4);
 
     // mutable with chaining functions
@@ -72,11 +73,194 @@ fn main() {
     println!("Round 1.45 = {}", 1.45f64.round());
     println!("Floor 1.45 = {}", 1.45f64.floor());
     println!("ceiling 1.45 = {}", 1.45f64.ceil());
-    println!("abs(-4) = {}", neg_4.abs());
-    println!("abs(-4) = {}", neg_4.abs());
-    println!("abs(-4) = {}", neg_4.abs());
-    println!("abs(-4) = {}", neg_4.abs());
-    println!("abs(-4) = {}", neg_4.abs());
+    println!("e ^ 2 = {}", 2f64.exp());
+    println!("log(2) = {}", 2f64.ln());
+    println!("log10(2) = {}", 2f64.log10());
+    println!("90 to Radians = {}", 90f64.to_radians());
+    println!("PI to degrees = {}", 3.14f64.to_degrees());
+    println!("Max 4, 5 = {}", 4f64.max(5f64));
+    println!("Min 4, 5 = {}", 4f64.min(5f64));
+    println!("Sin 3.14 = {}", 3.14f64.sin());
 
+    // conditional operators != > < == >= <= && || !
+    let age_old = 6;
+
+    if(age_old == 5){
+        println!("Go to Kindergarten");
+    } else if(age_old > 5) && (age_old <= 18){
+        println!("Go to grade {}", (age_old - 5))
+    } else if(age_old <= 25) && (age_old > 18){
+        println!("Go to college");
+    } else {
+        println!("Do what you want");
+    }
+
+    println!("!true = {}", !true);
+    println!("true || false = {}", true || false);
+    println!("true != false : {}", (true != false));
+    // make a turnary operator
+    let can_vote = if (age_old >= 18) {true} else {false};
+    println!("Can vote: {}", can_vote);
+
+    // looping
+    let mut x = 1;
+
+    loop {
+        if((x % 2) == 0){
+            println!("{}", x);
+            x += 1;
+
+            continue;
+        }
+        if(x > 10){
+            break;
+        }
+        x += 1;
+        continue;
+    }
+
+    //while loop
+    let mut y = 1;
+
+    while y <= 10 {
+        println!("{}", y);
+        y += 1;
+    }
+
+    // for loop
+    for z in 1..10 {
+        println!("FOR: {}", z);
+    }
+
+    // strings
+    let rand_string = "I am a random string";
+
+    println!("Length : {}", rand_string.len());
+
+    let (first, second) = rand_string.split_at(6);
+    println!("First : {}\nSecond : {}", first, second);
+
+    // iterator for string
+    let count = rand_string.chars().count();
+    let mut chars = rand_string.chars();
+
+    let mut indiv_char = chars.next();
+
+    loop {
+        match indiv_char {
+            Some(x) => println!("{}", x),
+            None => break,
+        }
+        indiv_char = chars.next();
+    }
+
+    // returning individual words in a string
+    let mut iter = rand_string.split_whitespace();
+
+    let mut indiv_word = iter.next();
+
+    loop {
+        match indiv_word {
+            Some(x) => println!("{}", x),
+            None => break,
+        }
+        indiv_word = iter.next();
+    }
+
+    // returning individual lines in a string
+    let rand_string2 = "I am a random string\nThere are other strings like it\nThis string is the best";
+
+    let mut lines = rand_string2.lines();
+    let mut indiv_line = lines.next();
+    loop {
+        match indiv_line {
+            Some(x) => println!("{}", x),
+            None => break,
+        }
+        indiv_line = lines.next();
+    }
+
+    println!("Find Best : {}", rand_string2.contains("best"));
+
+    // number guesing game, name loop
+    // 'outer: loop{
+    //     let number: i32 = 10;
+    //     println!("Pick a number");
+
+    //     loop {
+    //         let mut line = String::new();
+
+    //         let input = stdin().read_line(&mut line);
+
+    //         let guess: Option<i32> = input.ok().map_or(None, |_| line.trim().parse().ok());
+
+    //         match guess {
+    //             None => println!("Enter a number"),
+    //             Some(n) if n == number => {
+    //                 println!("You guessed it");
+    //                 // break outer loop
+    //                 break 'outer;
+    //             }
+    //             Some(n) if n < number => println!("Too Low"),
+    //             Some(n) if n > number => println!("Too High"),
+    //             Some(_) => println!("Error")
+    //         }
+    //     }
+    // }
+
+    // lists check out more on arrays in rust docs
+    let rand_array = [1,2,3];
+
+    println!("{}", rand_array[0]);
+
+    println!("{}", rand_array.len());
+
+    println!("Second 2 : {:?}", &rand_array[1..3]);
+
+    //vectors, unlike arrays vectors can grow in size
+    let mut vect1 = vec![1,2,3,4,5];
+    println!("Item 2 : {}", vect1[1]);
+
+    for i in &vect1 {
+        println!("Vect: {}", i);
+    }
+
+    vect1.push(6);
+
+    vect1.pop();
+
+    // tuples, chek out more on tuples in rust docs
+    let rand_tuple = ("Bobby", 40);
+
+    let rand_tuple_2: (&str, i8) = ("Bobby", 40);
+
+    println!("Name : {}", rand_tuple_2.0);
+
+    say_hello("Bobby");
+
+    println!("5 + 4 = {}", get_sum(5, 4));
+
+    let sum = get_sum;
+    println!("6 + 4 = {}", sum(6,4));
+
+    // closures, can be passed into a function
+    let sum_nums = |x: i32, y: i32| x + y;
+    println!("7 + 8 = {}", sum_nums(7,8));
+
+    // can access variables outside of closures, but with functions cannot
+    let num_ten = 10;
+
+    let add_10 = |x: i32| x + num_ten;
+    println!("5 + 10 = {}", add_10(5));
 
 }
+
+// functions, created outside of main()
+fn say_hello(name: &str){
+    println!("Hello {}!", name);
+}
+
+fn get_sum(num1: i32, num2: i32) -> i32 {
+    num1 + num2
+}
+
