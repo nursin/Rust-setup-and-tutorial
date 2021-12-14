c4299d79a4f1a5f2e28d1b7d7fc216ba184e12eb
Bobby Keel <bobbykeel76@gmail.com>
2021-12-14 07:31:45 -0600

    completed rust tut, enumerators, structs, more

--- src/main.rs
+++ src/main.rs
@@ -253,6 +253,49 @@ fn main() {
     let add_10 = |x: i32| x + num_ten;
     println!("5 + 10 = {}", add_10(5));
 
+    // ownership, pointers as references
+    
+    // none primitive value, cannot access moved nonprim value
+    // let vect1 = vec![1,2,3];
+    // let vect2 = vect1;
+    // println!("vect1[0] : {}", vect1[0]);
+
+    // primitive values, can access moved prim val
+    let prim_val = 1;
+    let prim_val2 = prim_val;
+    println!("prim_val : {}", prim_val);
+
+
+    let vect2 = vec![1,2,3];
+    println!("Sum of Vect: {}", sum_vects(&vect2));
+    println!("Vect : {:?}", vect2);
+
+    // create circle
+    let mut circle1 = Circle {
+        x: 10.0, y: 20.0, radius: 18.0
+    };
+
+    println!("X : {}, Y : {}, R: {}", circle1.x, circle1.y, circle1.radius);
+
+    println!("Circle Radius : {}", get_radius(&circle1));
+
+    println!("Circle X : {}", circle1.get_x());
+
+    println!("Circle Area : {}", circle1.area());
+
+    let mut rect1 = Rectangle {
+        height: 20.0, width: 10.0
+    };
+
+    println!("Rectangle Area : {}", rect1.area());
+
+    let hulk = Hero::Strong(100);
+    let quicksilver = Hero::Fast;
+    let spiderman = Hero::Info {name: "Spiderman".to_owned(), secret: "Peter Parker".to_owned()};
+
+    get_info(hulk);
+    get_info(spiderman);
+
 }
 
 // functions, created outside of main()
@@ -264,3 +307,64 @@ fn get_sum(num1: i32, num2: i32) -> i32 {
     num1 + num2
 }
 
+fn sum_vects(v1: &Vec<i32>) -> i32 {
+    let sum = v1.iter().fold(0, |mut sum, &x| {sum += x; sum});
+    return sum;
+}
+
+// structs
+struct Circle {
+    x: f64,
+    y: f64,
+    radius: f64,
+}
+
+fn get_radius(circle: &Circle) -> f64 {
+    circle.radius
+}
+
+// pub makes this fiuntion public then it can be accessed outside the function
+impl Circle {
+    pub fn get_x(&self) -> f64 {
+        self.x
+    }
+}
+
+// traits
+struct Rectangle {
+    height: f64,
+    width: f64,
+}
+
+trait HasArea {
+    fn area(&self) -> f64;
+}
+
+impl HasArea for Circle {
+    fn area(&self) -> f64 {
+        3.14159 * (self.radius * self.radius)
+    }
+}
+
+impl HasArea for Rectangle {
+    fn area(&self) -> f64 {
+        self.height * self.width
+    }
+}
+
+// enumerated types 1 of several values
+enum Hero {
+    Fast,
+    Strong(i32),
+    Info {name: String, secret: String}
+}
+
+fn get_info(h: Hero){
+    match h {
+        Hero::Fast => println!("Fast"),
+        Hero::Strong(i) => println!("Lifts {} tons", i),
+        Hero::Info {name, secret} => {
+            println!("{} is {}", name, secret);
+        },
+    }
+}
